ed7e9656c5acd5e481434ab09484feb679bce867
HermitF <fantasrulai@gmail.com>
2016-10-26 20:04:02 +0800

    build/hex

--- ino/201610/dev/build/core/abi.cpp.d
+++
@@ -1,2 +0,0 @@
-C:\Users\HermitF\AppData\Local\Temp\builddd3c988f282e0b5b408d55ba5f689ee0.tmp\core\abi.cpp.o: \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino\abi.cpp
--- ino/201610/dev/build/core/wiring.c.d
+++
@@ -1,6 +0,0 @@
-C:\Users\HermitF\AppData\Local\Temp\builddd3c988f282e0b5b408d55ba5f689ee0.tmp\core\wiring.c.o: \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino\wiring.c \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino\wiring_private.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino\Arduino.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino\binary.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h
